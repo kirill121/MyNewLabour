b74c696c6b618f8af6d83b1c66af919e0ac85cd2
kirill121 <kirill121@yahoo.com>
2017-07-07 13:08:44 +0200

    'employer_controller_functions'

---
+++ controllers/employer_controller.js
@@ -0,0 +1,32 @@
+const Employer = require('../models/employers');
+
+module.exports = {
+
+	viewEmployers: function(req, res){
+
+	},
+
+	viewEmployer: function(req, res){
+
+	},
+
+	viewAddEmployer: function(req, res){
+
+	},
+
+	viewUpdateEmployer: function(req, res){
+
+	},
+
+	addEmployer: function(req, res){
+
+	},
+
+	updateEmployer: function (req, res){
+
+	},
+
+	deleteEmployer: function(req, res){
+
+	}
+};
