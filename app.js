316aaab8f68a39c7c68ca806bbd11b833109da4d
kirill <kirill121@yahoo.com>
2018-04-12 11:52:08 +0200

    'latest'

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser')
 const authentication = require('./controllers/authentication');
 const path = require('path');
 const Employer = require('./models/employers')
+const nodemailer = require("nodemailer");
 
 const passport = require('passport');
 const passportService = require('./services/passport');
@@ -32,6 +33,58 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
+
+var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "k.kholine@elium.academy",
+        pass: ""
+    }
+});
+
+var rand, mailOptions, host, link;
+
+app.get('/empsignup', function(req, res, next){
+	console.log(req.query.to)
+    rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link='http://localhost:3000/login?id='+rand
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+        console.log(error);
+     }else{
+        console.log("Message sent: " + response.message);
+         }
+	});
+});
+
+app.get('http://localhost:3000/login?id='+rand, function(req,res){
+	console.log(req.protocol+":/"+req.get('host'));
+	if((req.protocol+"://"+req.get('host'))==("http://"+host))
+	{
+		console.log("Domain is matched. Information is from Authentic email");
+		if(req.query.id==rand)
+		{
+			console.log("email is verified");
+			res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+		}
+		else
+		{
+			console.log("email is not verified");
+			res.end("<h1>Bad Request</h1>");
+		}
+	}
+		else
+		{
+			res.end("<h1>Request is from unknown source");
+		}
+	});
 
 app.get('/', function(req, res) {	
 	var user;
